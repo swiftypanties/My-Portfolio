dec66bf12ac88b722106faa93311720223ec657f
Aviv David Malka <sonyon.winx@gmail.com>
2025-09-06 15:46:47 +0300

    project setup complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from "./assets/Logo.png"
+
+
+function App() {
+  return (
+    <>
+      <div>
+       <img src={logo} alt="logo" />
+      </div>
+    </>
+  )
+}
+
+export default App
